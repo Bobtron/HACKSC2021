fa5ee7478a68583f3be069c29e97859551e1eea3
Edison <evsiu@usc.edu>
2021-02-20 21:37:33 -0800

    Onlystans update, rated E for everyone

---
+++ XPR_test_fuck_it.js
@@ -0,0 +1,40 @@
+const RippleAPI = require('ripple-lib').RippleAPI
+// TESTNET ADDRESS 1
+const ADDRESS_1 = 'rpSDxPwUyUzyFxAyVGwGt1hoJGC8neLZhF'
+const SECRET_1 = "sh7ePwac3g2Py36YnnzTqJYxrEJpR"
+// TESTNET ADDRESS 2
+const ADDRESS_2 = "rBemDgBYvHhGXcuJBMMatQLEAaRKgBc32g"
+const instructions = {maxLedgerVersionOffset: 5}
+const currency = 'XRP'
+const amount = '100'
+const payment = {
+  source: {
+    address: ADDRESS_1,
+    maxAmount: {
+      value: amount,
+      currency: currency
+    }
+  },
+  destination: {
+    address: ADDRESS_2,
+    amount: {
+      value: amount,
+      currency: currency
+    }
+  }
+}
+const api = new RippleAPI({
+  //server: 'wss://s1.ripple.com'                 // MAINNET
+  server: 'wss://s.altnet.rippletest.net:51233'   // TESTNET
+})
+api.connect().then(() => {
+  console.log('Connected...')
+  api.preparePayment(ADDRESS_1, payment, instructions).then(prepared => {
+    const {signedTransaction, id} = api.sign(prepared.txJSON, SECRET_1)
+    console.log(id)
+    api.submit(signedTransaction).then(result => {
+      console.log(JSON.stringify(result, null, 2))
+      api.disconnect()
+    })
+  })
+}).catch(console.error)
---
+++ XRP_transaction.js
@@ -0,0 +1,54 @@
+const RippleAPI = require('ripple-lib').RippleAPI;
+
+const api = new RippleAPI({
+  server: 'wss://s1.ripple.com' // Public rippled server hosted by Ripple, Inc.
+});
+api.on('error', (errorCode, errorMessage) => {
+  console.log(errorCode + ': ' + errorMessage);
+});
+api.on('connected', () => {
+  console.log('connected');
+});
+api.on('disconnected', (code) => {
+  // code - [close code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent by the server
+  // will be 1000 if this was normal closure
+  console.log('disconnected, code:', code);
+});
+api.connect().then(() => {
+  /* insert code here */
+
+    const my_addr = 'r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59';
+    const rec_addr = 'rMH4UxPrbuMa1spCBR98hLLyNJp4d8p4tM';
+    const payment = {
+    "source": {
+        "address": "r9cZA1mLK5R5Am25ArfXFmqgNwjZgnfk59",
+        "maxAmount": {
+        "value": "0.01",
+        "currency": "USD",
+        "counterparty": "rMH4UxPrbuMa1spCBR98hLLyNJp4d8p4tM"
+        }
+    },
+    "destination": {
+        "address": "rpZc4mVfWUif9CRoHRKKcmhu1nx2xktxBo",
+        "amount": {
+        "value": "0.01",
+        "currency": "USD",
+        "counterparty": "rMH4UxPrbuMa1spCBR98hLLyNJp4d8p4tM"
+        }
+    }
+    };
+    return api.preparePayment(address, payment).then(prepared => {
+        /* ... */
+    }).catch(error => {
+        /* ... as with all prepare* methods, use a Promise catch block to handle errors ... */\
+        console.log("errooooooooooooooooooor")
+    })
+
+
+
+
+
+
+}).then(() => {
+    return api.disconnect();
+  }).catch(console.error);
